6e8d77909bfc0e564e49a42325e8e17d1038c791
LedesmaFran <fledesma@itba.edu.ar>
2020-02-10 23:17:22 -0300

    funcionamiento basico, para nada prolijo

--- qr/urls.py
+++ qr/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('<id_event>/', views.qr_reader, name='qr_reader'),
-    path('check/', views.check_data, name='check')
+    path('<id_event>/<id_user>/ok/',views.ok, name='ok'),
+    path('<id_event>/<id_user>/accredited/',views.accredited, name='accredited'),
+    path('<id_event>/no/',views.no, name='no')
     ]
--- qr/views.py
+++ qr/views.py
@@ -11,19 +11,29 @@ def qr_reader(request, id_event):
     selected_event = Event.objects.get(id=id_event)
     if not request.user.is_authenticated:
         return redirect('home')
-
+    if request.POST.get('cam_read'):
+        id_user = request.POST.get('cam_read')
+        person = participant.objects.get(id=id_user)
+        if selected_event.registered.filter(id=id_user):
+            selected_event.registered.remove(person)
+            selected_event.accredit.add(person)
     return render(request, "qr.html", {'event': selected_event})
 
 
-def check_data(request):
-    id_user = request.POST.get('cam_read')
+def ok(request, id_event, id_user):
+    if not request.user.is_authenticated:
+        return redirect('home')
     print(id_user)
-    id_event = request.POST.get('event')
-    print(id_event)
-    event = Event.objects.get(id=id_event)
-    if event.registered.filter(id=id_user):
-        person = participant.objects.filter(id=id_user)
-        print(person)
-        return JsonResponse("ok")
-    else:
-        return HttpResponse("fallo")
+    return render(request, "ok.html")
+
+
+def accredited(request, id_event, id_user):
+    if not request.user.is_authenticated:
+        return redirect('home')
+    person = participant.objects.get(id=id_user)
+    return render(request, "accredited.html", {"person": person})
+
+def no(request,id_event):
+    if not request.user.is_authenticated:
+        return redirect('home')
+    return render(request, "no.html")
